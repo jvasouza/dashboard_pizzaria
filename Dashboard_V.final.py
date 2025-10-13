9a7d40a00c25e3765c0cb5f1776ba502b1279a66
jvasouza <jvandrades02@gmail.com>
2025-10-13 14:19:56 -0300

    locale

--- Dashboard_V.final.py
+++ Dashboard_V.final.py
@@ -289,10 +289,6 @@ else:
 
 tab1, tab2, tab3 = st.tabs(["Faturamento", "Pedidos", "CMV"])
 
-try:
-    locale.setlocale(locale.LC_TIME, "pt_BR.UTF-8")
-except:
-    locale.setlocale(locale.LC_TIME, "Portuguese_Brazil.1252")
 
 # ==========================================================
 # ABA FATURAMENTO
